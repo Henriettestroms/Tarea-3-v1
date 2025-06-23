4e34483c953523188eb42defa9997d99df1c6a2a
Henriettestroms <hest@stud.ntnu.no>
2025-06-22 19:59:44 -0500

    etapa 2

--- cpp/include/VideoFollower.h
+++ cpp/include/VideoFollower.h
@@ -2,14 +2,17 @@
 #define VIDEOFOLLOWER_H
 #include "Subscriber.h"
 #include <QWidget>
+#include <QString>
 class QPushButton;
 class VideoFollower : public Subscriber {
 public:
     VideoFollower(const std::string& name, const std::string& topicName);
     void update(const std::string& message) override;
     QWidget* getView();
+
 private:
     QWidget* view{nullptr};
     QPushButton* video{nullptr};
+    QString lastUrl; 
 };
 #endif
--- cpp/src/VideoFollower.cpp
+++ cpp/src/VideoFollower.cpp
@@ -1,6 +1,9 @@
 #include "VideoFollower.h"
 #include <QHBoxLayout>
 #include <QPushButton>
+#include <QMediaPlayer>
+#include <QVideoWidget>
+#include <QUrl>
 
 VideoFollower::VideoFollower(const std::string& name, const std::string& topicName)
     : Subscriber(name, topicName)
@@ -9,10 +12,25 @@ VideoFollower::VideoFollower(const std::string& name, const std::string& topicNa
     auto* layout = new QHBoxLayout(view);
     video = new QPushButton(QString::fromStdString(name));
     layout->addWidget(video);
+    
+    QObject::connect(video, &QPushButton::clicked, [this]() {
+        if(lastUrl.isEmpty())
+            return;
+
+        auto* player = new QMediaPlayer;
+        auto* widget = new QVideoWidget;
+        player->setVideoOutput(widget);
+        player->setSource(QUrl(lastUrl));
+        widget->setAttribute(Qt::WA_DeleteOnClose);
+        widget->resize(640, 480);
+        widget->show();
+        player->play();
+    });
 }
 
 void VideoFollower::update(const std::string& message){
-    video->setText(QString::fromStdString(message));
+        lastUrl = QString::fromStdString(message);
+    video->setText(lastUrl);
 }
 
 QWidget* VideoFollower::getView(){
